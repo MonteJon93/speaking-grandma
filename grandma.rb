606fb37bee22841f42280c0fe7a1ab9e49685bf8
Jonathan Montesdeoca <JMontesdeoca93@gmail.com>
2020-12-10 16:37:40 -0500

    commit tree 57cf9954b45c99a1c3b40eef4bd0cabe0925f45d

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,21 @@
+
+def speak_to_grandma(phrase)
+    
+    if phrase == "I LOVE YOU GRANDMA!"
+        return "I LOVE YOU TOO PUMPKIN!"
+    elsif phrase != phrase.upcase
+        return "HUH?! SPEAK UP, SONNY!"
+    else phrase == phrase.upcase
+        return "NO, NOT SINCE 1938!"
+    end
+end
+
+
+
+
+
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
@@ -10,4 +28,6 @@
 # NO, NOT SINCE 1938!
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
+# 'I LOVE YOU TOO PUMPKIN!
+
+
